9ef957c6029935edfe2f4c15e893fbc99c32ddbc
Nikolai <nikolai.ivanov3110@gmail.com>
2023-07-13 11:48:55 +0400

    feat: updateRoom, createRoom
    add interface

--- src/sender/index.ts
+++ src/sender/index.ts
@@ -1,5 +1,5 @@
 import { CustomWebSocket, Request } from '../types';
-import { registerPlayer } from '../data/index';
+import { placeShip, registerPlayer, resetRoomUsers, gameSession, roomRegister, roomUsers } from '../data/index';
 import { players } from '../data/index';
 import { wsclients } from '../../index';
 
@@ -8,67 +8,133 @@ export const sendToAllClients = (message: Request, wsclients:CustomWebSocket[]) 
         client.send(JSON.stringify(message));
     });
 };
+
+const getPlayerNameByIndex = (index: string): string => {
+  const player = players.find((player) => player.index === index);
+  if (player) {
+    return player.name;
+  } else {
+    throw new Error(`Player not found with index ${index}`);
+  }
+};
+
 export const userRegistration = (receivedMessage: Request, ws:CustomWebSocket) => {
     const {name, password} = JSON.parse(receivedMessage.data);
-    const updatedMessage : Request = {
-    type: 'reg',
-    data: JSON.stringify({
-        name,
-        index: ws.index,
-        error: false,
-        errorText: '',
-    }),
-    id: 0,
-};
-ws.send(JSON.stringify(updatedMessage));
-registerPlayer(name, password, ws.index);
+    const existingPlayer = players.find((player) => player.name === name);
+    if (existingPlayer){
+        const updatedMessage : Request = {
+            type: 'reg',
+            data: JSON.stringify({
+                name,
+                index: ws.index,
+                error: true,
+                errorText: 'Player already exists',
+            }),
+            id: 0,
+        };
+
+        ws.send(JSON.stringify(updatedMessage));
+    } else {
+        const updatedMessage : Request = {
+            type: 'reg',
+            data: JSON.stringify({
+                name,
+                index: ws.index,
+                error: false,
+                errorText: '',
+            }),
+            id: 0,
+        };
+        ws.send(JSON.stringify(updatedMessage));
+        registerPlayer(name, password, ws.index);
+    }
 };
 
-export const createGame = (ws:CustomWebSocket ) => {
-    const updatedMessage : Request = {
-        type: 'create_game',
-        data: JSON.stringify({
-            idGame: players[0].index,
-            idPlayer: ws.index
-        }),
-        id: 0,
-    };
-    sendToAllClients(updatedMessage, wsclients);
-};
-
-export const updateRoom = () => {
-    const roomUsers = players.map((player) => ({
-        name: player.name,
-        index: player.index,
-      }));
-    const rooms = JSON.stringify([{
-        roomId: 1,
-        roomUsers: roomUsers,
+export const updateRoom = (ws:CustomWebSocket) => {
+  if (roomUsers.length === 0) {
+      const name = getPlayerNameByIndex(ws.index);
+      roomRegister(name, ws.index);
+      const rooms = JSON.stringify([{
+          roomId: ws.index,
+          roomUsers: roomUsers,
       }]);
 
-    const updatedMessage : Request = {
-        type: 'update_room',
-        data: rooms,
-        id: 0,
-    };
-    sendToAllClients(updatedMessage, wsclients);
+      const updatedMessage : Request = {
+          type: 'update_room',
+          data: rooms,
+          id: 0,
+      };
+      sendToAllClients(updatedMessage, wsclients);
+  }
 };
 
-export const startGame = (ws:CustomWebSocket, receivedMessage: Request) => {
-    const { gameId, ships, indexPlayer} = JSON.parse(receivedMessage.data);
-    console.log(gameId);
-    console.log(indexPlayer);
-    if (players.length === 2) {
-    const updatedMessage: Request = {
+export const createGame = (ws:CustomWebSocket, idGame: number ) => {
+  let playerId = 0;
+    if (roomUsers.length === 1) {
+        const name = getPlayerNameByIndex(ws.index);
+        const isIndexRegistered = roomUsers.some((user) => user.index === ws.index);
+        if (!isIndexRegistered) {
+            roomRegister(name, ws.index);
+            const filteredClients = wsclients.filter((client) => roomUsers.some((user) => user.index === client.index));
+            filteredClients.forEach((client) => {
+            playerId++;
+            const updatedMessage: Request = {
+                type: 'create_game',
+                data: JSON.stringify({
+                idGame: idGame,
+                idPlayer: playerId,
+                }),
+                id: 0,
+            };
+            client.send(JSON.stringify(updatedMessage));
+            resetRoomUsers();
+            });
+        }
+      }
+};
+
+
+
+export const startGame = (ws: CustomWebSocket, receivedMessage: Request) => {
+
+    const {gameId, ships} = JSON.parse(receivedMessage.data);
+    const updatedMessage : Request = {
         type: 'start_game',
         data: JSON.stringify({
-            ships,
-            currentPlayerIndex: ws.index,
+          ships,
+          currentPlayerIndex: ws.index,
         }),
         id: 0,
     };
     ws.send(JSON.stringify(updatedMessage));
-} else {
-    console.log('Невозможно начать игру. Необходимо два игрока.');
-}
+    placeShip(gameId, ws.index, ships);
+    console.log(gameSession);
+};
+
+export const userAttack = (ws: CustomWebSocket, receivedMessage: Request) => {
+  const { x, y } = JSON.parse(receivedMessage.data);
+  const updatedMessage: Request = {
+      type: 'attack',
+      data: JSON.stringify({
+          position: {
+              x: x,
+              y: y,
+          },
+          currentPlayes: ws.index,
+          status: 'miss'||'killed'||'shot',
+      }),
+        id: 0,
+    };
+    ws.send(JSON.stringify(updatedMessage));
+};
+
+export const turnUser = (ws: CustomWebSocket) => {
+    const updatedMessage: Request = {
+        type: 'turn',
+        data: JSON.stringify({
+            currentPlayer: ws.index
+        }),
+        id:0,
+    };
+    ws.send(JSON.stringify(updatedMessage));
 };
